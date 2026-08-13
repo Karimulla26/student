884d757ae00d8cf4c7adfcd9f3c8fc4fdfaf0be4
Karimulla26 <syedkarimulla240@gmail.com>
2026-08-13 15:21:13 +0530

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,9 +1,33 @@
-public class Main{
-    public static void main(String[]args){
-        int num=8;
+import java.util.Scanner;
 
-        while(num<=78){
-            num++;
-        }
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main{
+    static Scanner syed=new Scanner(System.in);
+    static String collegeName=syed.next();
+    int studentId;
+    String studentName;
+    int branchId;
+    String branchName;
+    Main(int studentId,String studentName,int branchId,String branchName ){
+    this.studentId=studentId;
+    this.studentName=studentName;
+    this.branchId=branchId;
+    this.branchName=branchName;
     }
+    public static void main(String[]args){
+        Main obj=new Main(22359,"Karimulla syed",101,"computer science and desiagn");
+        Main obj2=new Main(22410,"SaiKumar",2204,"electric communication");
+        obj.display();
+        obj2.display();
+    }
+    public void display(){
+        System.out.println(" collegeName:"+ collegeName);
+        System.out.println("studentId:"+studentId);
+        System.out.println("studentName:"+studentName);
+        System.out.println("branchId:"+branchId);
+        System.out.println("branchName:"+branchName);
+    }
+
+
 }
